e6e8d7eca92d30498dcc6dedc25d872b29456fd7
Ratchathon Makmool <125650220+maxratchathon@users.noreply.github.com>
2025-07-22 14:19:08 +0700

    feat: payment status, send unit test

--- src/payments/controllers/payments/payments.controller.spec.ts
+++ src/payments/controllers/payments/payments.controller.spec.ts
@@ -1,18 +1,44 @@
+import { send } from 'process';
+import { Req } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentsController } from './payments.controller';
+import { query, Request, Response} from 'express';
+import { json } from 'stream/consumers';
 
 describe('PaymentsController', () => {
   let controller: PaymentsController;
+
+  const requestMock = {
+    query: {}
+  } as unknown as Request
+
+  const statusResponseMock = {
+    send: jest.fn((x) => x),
+  }
+
+  const responseMock = {
+    status: jest.fn((x) => statusResponseMock),
+    send: jest.fn((x) => x),
+  } as unknown as Response;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PaymentsController],
     }).compile();
 
+
     controller = module.get<PaymentsController>(PaymentsController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getPayments', () => {
+    it('should return a status of 400', () => {
+      controller.getPayments(requestMock, responseMock);
+      expect(responseMock.status).toHaveBeenCalledWith(400);
+      expect(statusResponseMock.send).toHaveBeenCalledWith({ msg: 'Count and page query parameters are required.' });
+    });
   });
 });
--- src/payments/controllers/payments/payments.controller.ts
+++ src/payments/controllers/payments/payments.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('payments')
-export class PaymentsController {}
+export class PaymentsController {
+
+    @Get()
+    getPayments(@Req() request: Request, @Res() response: Response) {
+    const {count, page} = request.query;
+    if (!count || !page) {
+        return response.status(400).send({ msg: 'Count and page query parameters are required.' });
+    } else {
+        return response.status(200).json({
+            message: `Payments retrieved successfully`,
+        });
+    }
+}
+}
